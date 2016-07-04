b665772f3b62948e566340ed5d62c4dbf647febb
manh1208 <manhnv1208@gmail.com>
2016-07-04 19:46:33 +0700

    [ManhNV] - Login layout

---
+++ app/src/main/java/com/linhv/eventhub/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.linhv.eventhub.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.linhv.eventhub.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
